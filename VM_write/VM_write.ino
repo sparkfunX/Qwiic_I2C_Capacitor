ba022264970d61c39d0343198793364e9b3b68fa
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2020-10-20 11:01:37 -0600

    Generate product page files

--- VM_write/VM_write.ino
+++ VM_write/VM_write.ino
@@ -6,10 +6,10 @@ void setup() {
   Wire.begin();
   Serial.begin(115200);
 
-  Serial.println("Write 0x123 to volatilte memory:");
+  Serial.println("Write 0x5 to volatilte memory:");
   Wire.beginTransmission(address);
-  Wire.write(0x01);
-  Wire.write(0x23);
+  Wire.write(0x00);
+  Wire.write(0x6A);
   Wire.endTransmission();
   
   //This is the read function
@@ -20,33 +20,33 @@ void setup() {
     Serial.println(temp, HEX);
   }
 
-  Serial.println("Write 0x7 to volatile memory:");
-  Wire.beginTransmission(address);
-  Wire.write(0x00);
-  Wire.write(0x07);
-  Wire.endTransmission();
-
-  //This is the read function
-  Wire.requestFrom(address, 4);
-  while(Wire.available()) {
-    uint8_t temp = Wire.read();
-    Serial.print("0x");
-    Serial.println(temp, HEX);
-  }
-
-  Serial.println("Write 0x1F9 to volatile memory:");
-  Wire.beginTransmission(address);
-  Wire.write(0x01);
-  Wire.write(0xF9);
-  Wire.endTransmission();
- 
-  //This is the read function
-  Wire.requestFrom(address, 4);
-  while(Wire.available()) {
-    uint8_t temp = Wire.read();
-    Serial.print("0x");
-    Serial.println(temp, HEX);
-  }
+//  Serial.println("Write 0x7 to volatile memory:");
+//  Wire.beginTransmission(address);
+//  Wire.write(0x00);
+//  Wire.write(0x07);
+//  Wire.endTransmission();
+//
+//  //This is the read function
+//  Wire.requestFrom(address, 4);
+//  while(Wire.available()) {
+//    uint8_t temp = Wire.read();
+//    Serial.print("0x");
+//    Serial.println(temp, HEX);
+//  }
+//
+//  Serial.println("Write 0x1F9 to volatile memory:");
+//  Wire.beginTransmission(address);
+//  Wire.write(0x01);
+//  Wire.write(0xF9);
+//  Wire.endTransmission();
+// 
+//  //This is the read function
+//  Wire.requestFrom(address, 4);
+//  while(Wire.available()) {
+//    uint8_t temp = Wire.read();
+//    Serial.print("0x");
+//    Serial.println(temp, HEX);
+//  }
 }
 
 void loop() {
